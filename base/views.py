57f07b9911f4b11432ab1ff849ca523ea9ec8489
Alex Kuznecov <alexander.kuznecov16@gmail.com>
2024-09-17 19:31:28 +0300

    new changes

--- base/views.py
+++ base/views.py
@@ -1,17 +1,15 @@
-from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
+from django.utils.translation import gettext as _
 
 from .models import Product, Company
 
-# Create your views here.
-
 def home(request):
-    products = getProducts()
+    products = get_translated_products()  # Получаем переведенные продукты для главной страницы
     company_info = get_company_info()
-    return render(request, 'home.html', {'products': products, 'company_info': company_info, 'products': products})
+    return render(request, 'home.html', {'products': products, 'company_info': company_info})
 
 def products(request):
-    products = getProducts()
+    products = get_translated_products()  # Получаем переведенные продукты для страницы списка продуктов
     company_info = get_company_info()
     return render(request, 'products.html', {'products': products, 'company_info': company_info})
 
@@ -20,12 +18,16 @@ def contacts(request):
     return render(request, 'contacts.html', {'company_info': company_info})
 
 def product_detail(request, id):
-    product = get_object_or_404(Product, id=id)
+    product = get_object_or_404(Product, id=id)  # Получаем продукт по id или возвращаем 404
     company_info = get_company_info()
     return render(request, 'product_detail.html', {'product': product, 'company_info': company_info})
 
-def getProducts():
+
+def get_translated_products():
     products = Product.objects.all()
+    for product in products:
+        product.name = _(product.name)
+        product.description = _(product.description)
     return products
 
 def get_company_info():
